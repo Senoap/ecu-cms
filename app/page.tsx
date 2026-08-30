31f4242c17c42ee665c5ff63dcfd5efb0bc6f875
Senoap <senoap.work@gmail.com>
2026-08-30 22:18:39 +0700

    Second refactoring for the missing await

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@
 import { readPublishedDB } from '@/lib/db'
 import HomeClient from '@/components/HomeClient'
 
-export default function HomePage() {
-  // Aman karena berjalan di Server (bisa akses fs)
-  const db = readPublishedDB() 
+export default async function HomePage() {
+  const db = await readPublishedDB()
 
   return <HomeClient db={db} />
 }
